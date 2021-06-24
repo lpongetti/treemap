37bfbd9f7f859bda542fb11d510d3d553e9e2fe8
lorenzo <lorenzo.pongetti@gmail.com>
2021-06-24 14:46:38 +0200

    Binary fix

--- lib/src/tiles/binary.dart
+++ lib/src/tiles/binary.dart
@@ -3,24 +3,26 @@ import 'package:treemap/src/treenode.dart';
 
 class Binary extends Tile {
   List<TreeNode>? _children;
-  late List<num?> _sums;
+  late List<num> _sums;
 
   @override
-  position(TreeNode node, double left, double top, double right, double bottom) {
+  position(
+      TreeNode node, double left, double top, double right, double bottom) {
     _children = node.children;
 
-    var sum = 0;
-    _sums = List<num>.generate(_children.length + 1, (index) {
+    num sum = 0;
+    _sums = List<num>.generate(_children!.length + 1, (index) {
       if (index == 0) return 0;
 
-      sum += _children[index - 1].value;
+      sum += _children![index - 1].value;
       return sum;
     });
 
     partition(0, _children!.length, node.value, left, top, right, bottom);
   }
 
-  partition(int i, int j, num? value, double left, double top, double right, double bottom) {
+  partition(int i, int j, num? value, double left, double top, double right,
+      double bottom) {
     if (i >= j - 1) {
       var node = _children![i];
       node.left = left;
@@ -30,19 +32,23 @@ class Binary extends Tile {
       return;
     }
 
-    num? valueOffset = _sums[i], valueTarget = (value! / 2) + valueOffset!, k = i + 1, hi = j - 1;
+    num? valueOffset = _sums[i],
+        valueTarget = (value! / 2) + valueOffset,
+        k = i + 1,
+        hi = j - 1;
 
     while (k! < hi!) {
       int mid = (k + hi).toInt() >> 1;
-      if (_sums[mid]! < valueTarget)
+      if (_sums[mid] < valueTarget)
         k = mid + 1;
       else
         hi = mid;
     }
 
-    if ((valueTarget - _sums[k - 1 as int]!) < (_sums[k as int]! - valueTarget) && i + 1 < k) --k;
+    if ((valueTarget - _sums[k - 1 as int]) < (_sums[k as int] - valueTarget) &&
+        i + 1 < k) --k;
 
-    var valueLeft = _sums[k]! - valueOffset, valueRight = value - valueLeft;
+    var valueLeft = _sums[k] - valueOffset, valueRight = value - valueLeft;
 
     if ((right - left) > (bottom - top)) {
       var xk = (left * valueRight + right * valueLeft) / value;
